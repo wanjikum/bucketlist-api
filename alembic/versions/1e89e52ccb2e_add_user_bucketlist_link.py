e17a4052e5c78a39974888bc97e1a8606bbeeb56
wanjikum <wanjikumillicent5@gmail.com>
2017-05-13 16:59:11 +0300

    [Feature] Add migrations

---
+++ alembic/versions/1e89e52ccb2e_add_user_bucketlist_link.py
@@ -0,0 +1,24 @@
+"""add user_bucketlist_link
+
+Revision ID: 1e89e52ccb2e
+Revises: 
+Create Date: 2017-05-13 16:37:57.398478
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1e89e52ccb2e'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
