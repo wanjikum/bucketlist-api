fc90dbb2c664e669f30a88dc54c852ba06e20989
wanjikum <wanjikumillicent5@gmail.com>
2017-05-15 16:36:41 +0300

    [chore] create more bucketlist testcases

--- tests/test_bucketlist.py
+++ tests/test_bucketlist.py
@@ -13,36 +13,78 @@ class BucketlistTestCase(unittest.TestCase):
         """Test API can create a bucketlist using POST request"""
         pass
 
+    def test_reject_create_bucketlist_if_unauthorized(self):
+        """Test API rejects creating a bucketlist if unauthorized"""
+        pass
+
     def test_create_bucketlist_item(self):
         """Test API can create a bucketlist using POST request"""
+        pass
+
+    def test_reject_create_bucketlist_item_if_unauthorized(self):
+        """Test API rejects creating a bucketlist item if unauthorized"""
         pass
 
     def test_get_all_bucketlists(self):
         """Test API can get all bucketlists using GET request"""
         pass
 
+    def test_api_rejects_get_all_bucketlists_if_unathorized(self):
+        """Test API rejects getting all bucketlists if unauthorized"""
+        pass
+
     def test_api_can_get_bucketlist_by_id(self):
         """Test API can get a single bucketlist using it's id"""
         pass
 
-    def test_list_a_bucketlist_with_all_bucketlist_items(self):
+    def test_api_rejects_getting_bucketlist_by_id_if_unathorized(self):
+        """Test API rejects getting all bucketlist by id if unauthorized"""
+        pass
+
+    def test_api_lists_a_bucketlist_with_all_bucketlist_items(self):
         """Test API can list a bucketlist with all bucketlist items"""
+        pass
+
+    def test_api_rejects_listing_bucketlist_contents_if_unauthorized(self):
+        """Test API rejects listing bucketlist content if unauthorized"""
         pass
 
     def test_edit_bucketlist(self):
         """Test API can edit an existing bucketlist using PUT request"""
         pass
 
+    def test_reject_edit_bucketlist_if_unauthorized(self):
+        """Test API rejects editing an existing bucketlist if anauthorized"""
+        pass
+
     def test_edit_bucketlist_item(self):
         """Test API can edit an existing bucketlist item using PUT request"""
+        pass
+
+    def test_reject_editing_bucketlist_item_if_unauthorized(self):
+        """
+        Test API rejects editing an existing bucketlist item if anauthorized
+        """
         pass
 
     def test_delete_bucketlist(self):
         """Test API can delete an existing bucketlist"""
         pass
 
+    def test_reject_deleting_bucketlist(self):
+        """
+        Test API rejects deleting an existing bucketlist if anauthorized
+        """
+        pass
+
     def test_delete_bucketlist_item(self):
         """Test API can delete an existing bucketlist item"""
+        pass
+
+    def test_reject_deleting_a_bucketlist_item(self):
+        """
+        Test API rejects deleting an existing bucketlist item if anauthorized
+        """
         pass
 
     def tearDown(self):
