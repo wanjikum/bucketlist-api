6335434abf5b7c61ac45bf2ca6fb29082a7ed1f7
wanjikum <wanjikumillicent5@gmail.com>
2017-05-21 11:07:29 +0300

    [chore] Add a constructor in the users model

--- app/models.py
+++ app/models.py
@@ -10,6 +10,13 @@ class UserModel(db.Model):
     email = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(30), nullable=False)
 
+    def __init__(self, first_name, last_name, email, password):
+        """user model constructor"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+
     def __repr__(self):
         """returning a printable version for the object"""
         return "<UserModel: {} {}>".format(self.first_name, self.last_name)
