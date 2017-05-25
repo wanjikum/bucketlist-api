c8530eb2b562aa11dadb0db3774315831a2d5bc7
wanjikum <wanjikumillicent5@gmail.com>
2017-05-25 16:31:31 +0300

    [chore] Add logging in run.py

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import logging
+import sys
 import os
 
 BASE_URL = os.path.abspath(os.path.dirname(__file__))
@@ -8,6 +10,8 @@ from app import create_app
 config_name = os.getenv('APP_SETTINGS')
 app = create_app(config_name)
 
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
