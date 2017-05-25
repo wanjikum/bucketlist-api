6c390f5df8b243e5fca81f093875bbde56d6e03f
wanjikum <wanjikumillicent5@gmail.com>
2017-05-25 17:17:56 +0300

    [chore] Update Heroku config

--- run.py
+++ run.py
@@ -6,8 +6,8 @@ BASE_URL = os.path.abspath(os.path.dirname(__file__))
 
 from app import create_app
 
-# config_name = "development"
-config_name = os.getenv('APP_SETTINGS')
+config_name = "production"
+# config_name = os.getenv('APP_SETTINGS')
 
 app = create_app(config_name)
 
